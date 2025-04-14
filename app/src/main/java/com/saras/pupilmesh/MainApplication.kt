d17adff6303e96948f0e540771907816e07af09c
Saras <sarasv0022@gmail.com>
2025-04-14 09:37:04 +0000

    Added coil imageloader factory

---
+++ app/src/main/java/com/saras/pupilmesh/MainApplication.kt
@@ -0,0 +1,30 @@
+package com.saras.pupilmesh
+
+import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.memory.MemoryCache
+import dagger.hilt.android.HiltAndroidApp
+import okio.Path.Companion.toOkioPath
+
+@HiltAndroidApp
+class MainApplication : Application(), SingletonImageLoader.Factory {
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader.Builder(context)
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, 0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("image_cache").toOkioPath())
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .build()
+    }
+
+}
