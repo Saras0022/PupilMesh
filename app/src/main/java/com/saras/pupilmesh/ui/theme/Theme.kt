5d68cddeaa5785db5b1167723f3905db2d35a441
Saras <sarasv0022@gmail.com>
2025-04-14 09:37:54 +0000

    Commit

--- app/src/main/java/com/saras/pupilmesh/ui/theme/Theme.kt
+++ app/src/main/java/com/saras/pupilmesh/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.saras.pupilmesh.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
